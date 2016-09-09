0b14c562222c69e3026a237125e7a8bfce6c7243
KingVizzy <vallery.salomon@gmail.com>
2016-09-09 17:21:06 -0400

    HelloWorld

---
+++ HelloWorld2/HelloWorld2/HelloWorld.C
@@ -0,0 +1,13 @@
+// EEGR 409 C Programming Applications
+// Vallery Salomon
+// Hello World Assignment
+//August 26, 2016
+#include <stdio.h>
+
+
+void main() {
+
+	printf("Hello World! \n"); //printf to display messages
+
+	getchar(); //getchar for console to wait for user input from keyboard
+}
